dbed26c3c6d6d2358becaed267d7aca46187dcee
Rand <avelytchko@gmail.com>
2016-02-12 17:11:03 +0200

    Rspec files fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'bundler/setup'
 require 'shoulda/matchers'
-::Bundler.require(:default, :test)
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
@@ -51,7 +50,7 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+  config.order = 'random'
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and

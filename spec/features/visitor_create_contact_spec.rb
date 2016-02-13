5461d34b8296189ef97bb2d7c4302ee2a9cf9540
Rand <avelytchko@gmail.com>
2016-02-13 15:55:12 +0000

    Add capybara test to contacts page

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,7 +3,14 @@ require 'spec_helper'
 feature 'Contact Creation' do
   scenario 'allow access to contact page' do
     visit '/contacts'
+    expect(page).to have_content I18n.t('contacts.contact_us')
+  end
 
-    expect(page).to have_content 'Contact us'
+  scenario 'allows a guest to create conect' do
+    visit '/contacts'
+    fill_in :contact_email, with: 'user@example.com'
+    fill_in :contact_message, with: 'something'
+    click_button 'Send Message'
+    expect(page).to have_content 'Thanks!'
   end
 end

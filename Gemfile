38e0162f136009fbfd2e69f6c6f20e184b45af9a
Rand <avelytchko@gmail.com>
2016-02-13 16:30:22 +0000

    Add database_cleaner gem

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ group :test, :development do
   gem 'rspec-rails'
   gem 'capybara'
   gem 'factory_girl_rails', '~> 4.0'
+  gem 'database_cleaner'
 end
 
 group :test do
---
+++ spec/features/user_creates_article_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature 'Article Creation' do
+  before(:all) do
+    sign_up
+  end
+  scenario 'allow user to visit new article page' do
+    visit new_article_path
+    expect(page).to have_content 'New Article'
+  end
+end
